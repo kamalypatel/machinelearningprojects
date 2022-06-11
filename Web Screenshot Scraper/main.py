c259638611b9fbb682fb39a0c403075696f1a4a8
Kamal <kamalpat@buffalo.edu>
2022-06-11 17:52:53 -0400

    commented out bad code

--- Web Screenshot Scraper/main.py
+++ Web Screenshot Scraper/main.py
@@ -4,11 +4,13 @@ import time
 DRIVER = 'chromedriver'
 driver = webdriver.Chrome(DRIVER)
 driver.get('https://earth.google.com/web/search/701+ellicotts+street/@42.89777795,-78.8675311,194.02888237a,122.5392609d,35y,0h,0t,0r/data=Cn8aVRJPCiUweDg5ZDMxMjU5NDk1M2ZjNjc6MHhlMzgxNWI1NWY3MjI2ZTJjGfGjda_uckVAIdCg_Dh1t1PAKhQ3MDEgZWxsaWNvdHRzIHN0cmVldBgCIAEiJgokCUFuplNGc0VAEY0GMwiXckVAGX2j6_oKt1PAISOeDXfft1PA')
-time.sleep(10)
+time.sleep(1)
 
-element = driver.find_element('zoom-in')
+"""
+element = driver.find_element(by=id, value='zoom-in')
 
 element.click()
+"""
 
 screenshot = driver.save_screenshot('my_screenshot.png')
 driver.quit()
