a141b926ac43c79eb78a3370efd08b1084f220c9
Kamal <kamalpat@buffalo.edu>
2022-06-12 12:41:00 -0400

    added the ability to read a csv of long lat values to scrape for screenshots and name the screenshots the address name

--- Web Screenshot Scraper/main.py
+++ Web Screenshot Scraper/main.py
@@ -2,22 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import csv
 
 
-DRIVER = 'chromedriver'
-driver = webdriver.Chrome(DRIVER)
-actions = ActionChains(driver)
-driver.set_window_size(1900, 1080)
-driver.get('https://earth.google.com/web/search/701+ellicotts+street/@42.89777795,-78.8675311,194.02888237a,122.5392609d,35y,0h,0t,0r/data=Cn8aVRJPCiUweDg5ZDMxMjU5NDk1M2ZjNjc6MHhlMzgxNWI1NWY3MjI2ZTJjGfGjda_uckVAIdCg_Dh1t1PAKhQ3MDEgZWxsaWNvdHRzIHN0cmVldBgCIAEiJgokCUFuplNGc0VAEY0GMwiXckVAGX2j6_oKt1PAISOeDXfft1PA')
-time.sleep(8)
-"""
-element = driver.find_element(By.CSS_SELECTOR, 'icon-icon')
+with open('long_lats.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        DRIVER = 'chromedriver'
+        driver = webdriver.Chrome(DRIVER)
+        actions = ActionChains(driver)
+        driver.set_window_size(1900, 1080)
+        url = "https://earth.google.com/web/search/" + str(row[1]) + "%09" + str(row[2]) + "/@" + str(row[1]) + "," + str(row[2]) + ",300.68548482a,1250.12036026d,3y,0h,0t,0r"
+        driver.get(url)
+        time.sleep(12)
+        driver.execute_script("document.body.style.zoom='85%'")
+        time.sleep(1)
 
-element.click()
-"""
+        screenshot = driver.save_screenshot(row[0] + ".png")
+        driver.quit()
 
-driver.execute_script("document.body.style.zoom='85%'")
-time.sleep(1)
-
-screenshot = driver.save_screenshot('my_screenshot.png')
-driver.quit()
+#left this commented part below because the format was hard to find online but this is the correct format
+#element = driver.find_element(By.CSS_SELECTOR, 'icon-icon')
+#element.click()
